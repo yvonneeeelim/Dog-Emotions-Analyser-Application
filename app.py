e27abf681ef92163fcc3c4a69de0a8690e997bf6
yvonneeeelim <141410050+yvonneeeelim@users.noreply.github.com>
2023-11-05 01:57:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ if selected_tab == "Upload your Pet Video":
     st.header("Upload your Pet Video")
 
     # Load the trained model
-    model = load_model('efficientnet_model.h5')
+    with open('efficientnet_model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
 
     def preprocess_frames(frames):
         processed_frames = []
@@ -120,7 +121,8 @@ elif selected_tab == "Upload your Pet Image":
     st.header("Upload your Pet Image")
    
     # Load the trained model
-    model = load_model('efficientnet_model.h5')
+    with open('efficientnet_model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
 
     # Get user input for image upload
     uploaded_file = st.file_uploader('Upload an image of your pet to understand its behaviour', type=['jpg', 'jpeg', 'png'])
